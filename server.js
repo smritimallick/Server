65b37688dceb64eb058c70461a8dc8cc0ef2e7f0
smritimallick <smritimallick0204@gmail.com>
2022-12-08 15:24:32 +0530

    adding nodemon

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require("http");
+const http = require("http"); // importing  pa ckage
 
 const port = 8081;
 
 http.createServer((request, response) => {
+    // callback function
     response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write("<h2> Hello, this is our server ^_^ </h2>");
+    response.write("<h2> Meow, welcome to our server  ^_^ </h2>");
     response.end();
 }).listen( port, () => {
+    // callback function
     console.log(`Nodejs Server started on ${port}`);
 });
+
+// open on http://localhost:8081 : requesting for server
